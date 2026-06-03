76d117e494b73db928769205c385efc83f49c66c
João Victor Viana Oliveira Silva <joaovctrjvv90@gmail.com>
2026-06-03 18:04:13 -0300

    Implementei melhorias no servidor.py, iniciar_sistema e testemvp - corrigi o backup - agora o mvp esta totalmente funcional

--- DOCS/iniciar_sistema.bat
+++ DOCS/iniciar_sistema.bat
@@ -5,23 +5,19 @@ echo ===============================
 echo INICIANDO MDR ECOSYSTEM
 echo ===============================
 
-REM 🔴 BACKEND (BANCO)
+REM BACKEND
 start cmd /k "echo BACKEND INICIADO && python server.py"
-
 timeout /t 3 >nul
 
-REM 🔵 FRONTEND (HTML)
+REM FRONTEND
 start cmd /k "echo FRONTEND INICIADO && python -m http.server 5500 --bind 10.100.20.142"
-
 timeout /t 3 >nul
 
-REM 🌐 ABRIR NO CHROME
+REM ABRIR NO CHROME DIRETO NO HTML PRINCIPAL
 start chrome http://10.100.20.142:5500/testemvp.html
 
 echo.
-echo ✅ SISTEMA PRONTO
-echo ✅ Banco ativo (porta 5000)
-echo ✅ Interface ativa (porta 5500)
-echo ✅ Abrindo navegador...
-
-exit
+echo SISTEMA PRONTO
+echo Banco ativo (porta 5000)
+echo Interface ativa (porta 5500)
+echo Abrindo navegador...
